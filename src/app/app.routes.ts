9d8a0615cae8b99df9f7aceea9ce631aa07c5361
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-09-08 16:42:33 +0700

    component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'user', component: UserComponent },
+    { path: 'employee', component: EmployeeComponent},
 ];
