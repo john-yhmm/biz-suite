e1037b2681ad43d852a1e91b4cf20860a41afa67
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-09-08 17:45:19 +0700

    added a form

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss'
 })
 export class EmployeeComponent {
+  employeeForm = new FormGroup({
+    id : new FormControl(""),
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
 
+  submit() {
+    alert("Welcome back, " + this.employeeForm.value.name + "🎉");
+  }
 }
