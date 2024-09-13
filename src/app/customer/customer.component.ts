622b3e0cbcf14340bbb26af625ac03751d7be835
nooele2 <noellehtoo@gmail.com>
2024-09-13 19:03:50 +0700

    Form3

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-customer',
+  selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss'
 })
 export class CustomerComponent {
-
+  customer = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone_number: new FormControl(''),
+    address: new FormControl(''),
+  });
 }
