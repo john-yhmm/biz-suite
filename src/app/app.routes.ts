1903eb5f180c3feb26ba8400a2cb8dedb3a1144e
ChannMyae-Zaw <channmyaezaw0@gmail.com>
2024-09-16 21:10:50 +0700

    shipment
    shipment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { ShipmentComponent } from './shipment/shipment.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'user', component: UserComponent },
+    { path: 'shipment', component: ShipmentComponent}
 ];
