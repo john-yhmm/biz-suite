52043286fe7bd81cb2cf2b8beb4333f125f1627f
HTOO PYAE SHAN <pyaehtoo333@gmail.com>
2024-09-16 20:31:20 +0700

    invoice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { CustomerComponent } from './customer/customer.component';
+import {InvoiceComponent } from './invoice/invoice.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'user', component: UserComponent },
+    { path: 'customer', component: CustomerComponent},
+    { path: 'invoice', component: InvoiceComponent},
 ];
